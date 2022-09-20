455a7c4086ae39e8b82e5427b417f71d124aaa31
chohon <siaksiakx@gmail.com>
2022-09-20 15:14:43 +0900

    back-end basic

--- app.py
+++ app.py
@@ -12,5 +12,21 @@ db = client.jungletube
 def main():
     return render_template('main.html')
 
+@app.route('/api/login')
+def login():
+    return
+
+@app.route('/api/logout')
+def logout():
+    return
+
+@app.route('/api/signin')
+def signin():
+    return
+
+@app.route('/api/upload')
+def upload():
+    return
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
