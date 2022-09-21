ab6ab1b6cb6df644a2ab1e63f4846ec879c6175a
jamiehun <jamiesunghun@gmail.com>
2022-09-21 16:10:04 +0900

    Jinja2_update

--- mac_venv/app.py
+++ mac_venv/app.py
@@ -41,7 +41,7 @@ def showVideo():
         videoCount = len(video_list) # 비디오 개수 세기
         collectionCount = math.ceil(videoCount / 4) # 비디오 개수 4개로 자르기
         dummyCount = 4 - (videoCount % 4) # 비디오 개수 중 4의 배수가 아닌 수에 대한 dummy값 확인
-        card_count[i] = {'collectionCount' : collectionCount, 'dummyCount' : dummyCount}
+        card_count[i] = {'collectionCount' : collectionCount, 'dummyCount' : dummyCount, 'cardCount' : videoCount}
         
     # dict 값, dummy_count 값, 
     return render_template('child_jinja2.html', dict = dict, card_count = card_count)
