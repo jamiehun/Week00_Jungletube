27dc3b0411234fdf908a02c91119da111541ebe4
jamiehun <jamiesunghun@gmail.com>
2022-09-22 03:35:03 +0900

    button_Complete

--- mac_venv/app.py
+++ mac_venv/app.py
@@ -40,8 +40,8 @@ def showVideo():
         # 비디오 개수에 대한 정보 확인
         videoCount = len(video_list) # 비디오 개수 세기
         collectionCount = math.ceil(videoCount / 4) # 비디오 개수 4개로 자르기
-        dummyCount = 4 - (videoCount % 4) # 비디오 개수 중 4의 배수가 아닌 수에 대한 dummy값 확인
-        card_count[i] = {'collectionCount' : collectionCount, 'dummyCount' : dummyCount, 'cardCount' : videoCount}
+        dummyCount = lambda x : 4 - (x % 4) if x % 4 != 0 else 0 # 비디오 개수 중 4의 배수가 아닌 수에 대한 dummy값 확인
+        card_count[i] = {'collectionCount' : collectionCount, 'dummyCount' : dummyCount(videoCount), 'cardCount' : videoCount}
         
     # dict 값, dummy_count 값, 
     return render_template('child_jinja2.html', dict = dict, card_count = card_count)
